100f356860a7a4e33f81e5c85d1132850d8fc53a
wonderfulnewworld <shinsekaihibiki@gmail.com>
2026-08-14 02:32:59 -0700

    missing import moment

--- Content.Client/IoC/ClientContentIoC.cs
+++ Content.Client/IoC/ClientContentIoC.cs
@@ -29,6 +29,7 @@ using Content.Shared.Chat;
 using Content.Shared.IoC;
 using Content.Shared.Players.PlayTimeTracking;
 using Content.Shared.Players.RateLimiting;
+using Content.Shared._FarHorizons.Lobby;
 using Content.Client._FarHorizons.Lobby;
 using Content.Shared._Starlight;
 using Content.Client._NullLink;
